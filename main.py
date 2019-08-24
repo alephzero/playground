a161b4ee95ac8cbcbc77ca5e0b8b64b69a14b98f
Leonid Shamis <lshamis@thirdwave.ai>
2019-08-23 18:38:50 -0700

    Better UI. Still just py

--- main.py
+++ main.py
@@ -1,31 +1,82 @@
 import asyncio
-from aiohttp import web
+from aiohttp import web, WSMsgType, __version__
+import json
 import tempfile
+
+print('aiohttp.__version__', __version__)
+
+class Namespace:
+    pass
 
 async def root_handler(request):
     return web.FileResponse('/index.html')
 
-async def run_code(request):
-    with tempfile.NamedTemporaryFile() as code_file:
-        with open(code_file.name, 'w') as fd:
-            fd.write(await request.text())
-        proc = await asyncio.create_subprocess_exec(
-            'python3', code_file.name,
+class CodeRunner:
+    def __init__(self, cmd, ws_out):
+        self.cmd = cmd
+        self.ws_out = ws_out
+        self.proc = None
+        self.dead = False
+
+    async def run(self):
+        if self.dead:
+            return
+        self.code_file = tempfile.NamedTemporaryFile()
+        with open(self.code_file.name, 'w') as fd:
+            fd.write(self.cmd['code'])
+        # self.cmd['lang']
+        self.proc = await asyncio.create_subprocess_exec(
+            'python3', '-u', self.code_file.name,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE)
+        if self.dead:
+            await self.kill()
+            return
+        asyncio.ensure_future(self._stream(self.proc.stdout, 'stdout'))
+        asyncio.ensure_future(self._stream(self.proc.stderr, 'stderr'))
+        await self.proc.wait()
 
-        try:
-            stdout, stderr = await asyncio.wait_for(proc.communicate(), timeout=15)
-        except asyncio.TimeoutError:
-            proc.kill()
-            stdout, stderr = await proc.communicate()
+    async def kill(self):
+        self.dead = True
+        self.ws_out = None
+        if self.proc:
+            try:
+                self.proc.kill()
+            except:
+                pass
 
-    return web.json_response({
-        'stdout': stdout.decode('utf-8'),
-        'stderr': stderr.decode('utf-8'),
-    })
+    async def _stream(self, stream, name):
+        while True:
+            line = await stream.read(1024)
+            if not self.dead and line:
+                await self.ws_out.send_json({
+                    'stream': name,
+                    'output': line.decode('utf-8'),
+                })
+            else:
+                break
+
+async def run_code_handshake(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    ns = Namespace()
+    ns.runner = None
+    async for msg in ws:
+        if msg.type == WSMsgType.TEXT:
+            if msg.data == 'close':
+                await ns.runner.kill()
+            else:
+                ns.runner = CodeRunner(json.loads(msg.data), ws)
+                asyncio.ensure_future(ns.runner.run())
+        elif msg.type == WSMsgType.ERROR:
+            if ns.runner:
+                await ns.runner.kill()
+            break
+    if ns.runner:
+        await ns.runner.kill()
 
 app = web.Application()
 app.add_routes([web.get('/', root_handler),
-                web.post('/api/run', run_code)])
+                web.get('/api/run', run_code_handshake)])
 web.run_app(app, port=12385)
--- run.sh
+++ run.sh
@@ -3,10 +3,45 @@ cd "$(dirname "$0")"
 
 docker build -t alephzero_playground .
 
+opts="$(getopt -o n:p: -l name:,port:,ipc: --name "$0" -- "$@")"
+eval set -- "$opts"
+
+NAME=""
+PORT=12385
+IPC="--ipc=host"
+
+while true
+do
+  case "$1" in
+    -n|--name)
+      NAME="--name $2"
+      shift 2
+      ;;
+    -p|--port)
+      PORT=$2
+      shift 2
+      ;;
+    --ipc)
+      IPC="--ipc=$2"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Unknown flag: $1" >&2
+      exit 1
+      ;;
+  esac
+done
+
 docker run                         \
   --rm                             \
   -it                              \
+  $NAME                            \
+  -p $PORT:12385                   \
+  $IPC                             \
   -v ${PWD}/main.py:/main.py       \
   -v ${PWD}/index.html:/index.html \
-  -p 12385:12385                   \
   alephzero_playground
