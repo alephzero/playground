d0cda6024ae202094dcb6fff0d08ee9d65aa09f1
Leonid Shamis <leonid.shamis@gmail.com>
2019-09-08 10:37:31 -0700

    Remove linux-headers from deps

--- examples/pub.py
+++ examples/pub.py
@@ -9,7 +9,7 @@ p = a0.Publisher(tm.publisher_topic('location'))
 for i in range(10):
     payload = 'here (ts={})'.format(i)
     print('publishing:', payload)
-    p.pub(a0.Packet([], payload))
+    p.pub(payload)
     time.sleep(1)
 
 print('Done!')
